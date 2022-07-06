21500b8be061fb20ef03150a9a80d3b7f0d627da
Daniel <daniel.torres@owasp.org>
2022-07-06 01:54:05 -0400

    update

--- lanscanner.sh
+++ lanscanner.sh
@@ -1106,7 +1106,7 @@ if [[ $port_scanner = "nmap" ]] || [ $port_scanner == "nmap_masscan" ] || [ $por
 
 	if [ "$MODE" == "proxy" ]; then 
 		echo -e "[+] Realizando escaneo tcp (solo 100 puertos)" 			
-		proxychains nmap -sT -Pn -T4 --top-ports 100 -n -iL  $live_hosts -oG .escaneo_puertos/tcp-100-nmap.grep
+		proxychains nmap -sT -Pn -T4 --top-ports 1000 -n --open --min-parallelism 100 --min-rate 1   -iL  $live_hosts -oG .escaneo_puertos/tcp-100-nmap.grep
 		egrep -v "^#|Status: Up" .escaneo_puertos/tcp-100-nmap.grep | cut -d' ' -f2,4- | sed -n -e 's/Ignored.*//p'  | awk '{for(i=2; i<=NF; i++) { a=a" "$i; }; split(a,s,","); for(e in s) { split(s[e],v,"/"); printf "%s:%s\n" , $1, v[1]}; a="" }' | sed 's/ //g'  >>  .escaneo_puertos/tcp-ports.txt
 	else
 		echo -e "[+] Realizando escaneo de puertos especificos (informix, Web services)"  			
