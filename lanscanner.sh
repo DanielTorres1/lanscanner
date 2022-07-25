9a8510a710d1db309f57a240b39cc3a92b1a44f0
Daniel <daniel.torres@owasp.org>
2022-07-25 10:34:32 -0400

    update

--- lanscanner.sh
+++ lanscanner.sh
@@ -3247,7 +3247,7 @@ then
 			if [[ $free_ram -gt $min_ram && $perl_instancias -lt $max_perl_instancias  ]];then 
 			#echo "FILE $prefijo$IP_LIST_FILE"			
 				#################  Realizar el escaneo por dominio  ##############	
-				echo "FILEEE: $prefijo$IP_LIST_FILE"			
+				echo "FILEEE: $prefijo$IP_LIST_FILE DOMINIO_INTERNO $DOMINIO_INTERNO"			
 				if grep -q "," "$prefijo$IP_LIST_FILE" 2>/dev/null; then					
 					lista_subdominios=`grep --color=never $ip $prefijo$IP_LIST_FILE | egrep 'subdomain|vhost'| cut -d "," -f2 | grep --color=never $DOMINIO_INTERNO` | uniq
 					echo "lista_subdominios $lista_subdominios"
